6565a2bc9c0d3bcaef4a99cbfb293aafaccdcbf5
Jacob Upton <Jsupton@csbsju.edu>
2019-02-24 11:47:46 -0600

    Adding Solitaire

---
+++ Deck.cpp
@@ -0,0 +1,196 @@
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "Card.h"
+#include "Deck.h"
+#include "Gameboard.h"
+
+using namespace std;
+
+/**
+ * An object of type Deck represents an ordinary deck of 52 playing cards.
+ * The deck can be shuffled, and cards can be dealt from the deck.
+ * @author jsupton
+ * @author Wilmot Osei-Bonsu
+ * @version Febuary 16 2019
+ */
+
+    // An arrayList of the deck of cards
+    vector<Card> deck;
+
+    //A Number of cards for the deck
+    int NumberOfCards;
+
+    /**
+     * Default constructor. It simply creates a deck of 52 cards
+     */
+    Deck::Deck() {
+        // Create an unshuffled deck of cards.
+        deck.clear();
+        for (int suit = 0; suit <= 3; suit++) {
+            for (int value = 1; value <= 13; value++) {
+                deck.push_back(Card(value, suit));
+            }
+        }
+    }
+
+    /**
+     * Shuffles the deck using the Fisher-Yates algorithm
+     */
+    void Deck::shuffle() {
+        // Put all the used cards back into the deck, and shuffle it into
+        // a random order.
+
+        for (int i = 51; i > 0; i--) {
+
+            int random = rand()%(i+1);
+
+            Card temp = deck[i];
+            deck.at(i) = deck[random];
+            deck.at(random) = temp;
+        }
+    }
+
+    /**
+     * Creates a new shuffled deck of cards in an order that guarantees that the
+     * User will win their solitaire game
+     */
+    void Deck::shuffle_WIN() {
+        deck.clear();
+        deck.push_back(Card(1, 0));
+        deck.push_back(Card(2, 0));
+        deck.push_back(Card(1, 1));
+        deck.push_back(Card(4, 0));
+        deck.push_back(Card(3, 0));
+        deck.push_back(Card(1, 2));
+        deck.push_back(Card(5, 0));
+        deck.push_back(Card(4, 1));
+        deck.push_back(Card(3, 1));
+        deck.push_back(Card(1, 3));
+        deck.push_back(Card(6, 0));
+        deck.push_back(Card(5, 1));
+        deck.push_back(Card(4, 2));
+        deck.push_back(Card(3, 2));
+        deck.push_back(Card(2, 1));
+        deck.push_back(Card(7, 0));
+        deck.push_back(Card(6, 1));
+        deck.push_back(Card(6, 2));
+        deck.push_back(Card(5, 2));
+        deck.push_back(Card(3, 3));
+        deck.push_back(Card(2, 2));
+        deck.push_back(Card(7, 1));
+        deck.push_back(Card(7, 2));
+        deck.push_back(Card(7, 3));
+        deck.push_back(Card(6, 3));
+        deck.push_back(Card(5, 3));
+        deck.push_back(Card(4, 3));
+        deck.push_back(Card(2, 3));
+        for (int value = 8; value <= 13; value++) {
+            for (int suit = 0; suit <= 3; suit++) {
+                deck.push_back(Card(value, suit));
+            }
+        }
+    }
+
+    /**
+     * Creates a new shuffled deck of cards in an order that guarantees that the
+     * User will win their solitaire game
+     */
+    void Deck::shuffle_WIN2() {
+        deck.clear();
+        deck.push_back(Card(7, 0));
+        deck.push_back(Card(5, 0));
+        deck.push_back(Card(6, 1));
+        deck.push_back(Card(2, 1));
+        deck.push_back(Card(3, 0));
+        deck.push_back(Card(4, 1 ));
+        deck.push_back(Card(1, 3));
+        deck.push_back(Card(1, 2));
+        deck.push_back(Card(1, 1));
+        deck.push_back(Card(1, 0));
+        deck.push_back(Card(7, 1));
+        deck.push_back(Card(2, 0));
+        deck.push_back(Card(3, 1));
+        deck.push_back(Card(4, 0));
+        deck.push_back(Card(5, 1));
+        deck.push_back(Card(7, 3));
+        deck.push_back(Card(2, 2));
+        deck.push_back(Card(3, 3));
+        deck.push_back(Card(4, 2));
+        deck.push_back(Card(5, 3));
+        deck.push_back(Card(6, 0));
+        deck.push_back(Card(7, 2));
+        deck.push_back(Card(6, 3));
+        deck.push_back(Card(5, 2));
+        deck.push_back(Card(4, 3));
+        deck.push_back(Card(3, 2));
+        deck.push_back(Card(2, 3));
+        deck.push_back(Card(6, 2));
+        for (int value = 8; value <= 13; value++) {
+            for (int suit = 0; suit <= 3; suit++) {
+                deck.push_back(Card(value, suit));
+            }
+        }
+    }
+
+    /**
+     * Creates a new shuffled deck of cards in an order that guarantees that the
+     * User will lose their solitaire game
+     */
+    void Deck::shuffle_LOSE() {
+        deck.clear();
+        deck.push_back(Card(13, 0));
+        deck.push_back(Card(8, 0));
+        deck.push_back(Card(13, 1));
+        deck.push_back(Card(8, 0));
+        deck.push_back(Card(8, 0));
+        deck.push_back(Card(13, 2));
+        deck.push_back(Card(1, 0));
+        deck.push_back(Card(12, 1));
+        deck.push_back(Card(10, 1));
+        deck.push_back(Card(13, 3));
+        deck.push_back(Card(1, 0));
+        deck.push_back(Card(12, 1));
+        deck.push_back(Card(10, 2));
+        deck.push_back(Card(9, 2));
+        deck.push_back(Card(11, 1));
+        deck.push_back(Card(1, 0));
+        deck.push_back(Card(12, 1));
+        deck.push_back(Card(10, 2));
+        deck.push_back(Card(9, 2));
+        deck.push_back(Card(8, 3));
+        deck.push_back(Card(11, 2));
+        deck.push_back(Card(1, 1));
+        deck.push_back(Card(12, 2));
+        deck.push_back(Card(10, 3));
+        deck.push_back(Card(9, 3));
+        deck.push_back(Card(9, 3));
+        deck.push_back(Card(11, 3));
+        deck.push_back(Card(11, 3));
+        for (int value = 2; value <= 7; value++) {
+            for (int suit = 0; suit <= 3; suit++) {
+                deck.push_back(Card(value, suit));
+            }
+        }
+    }
+
+    /**
+     * Gets the deck of cards
+     * @return ArrayList<Cards> representing the current deck
+     */
+    vector<Card> * Deck::getDeck() {
+        return &this ->deck;
+    }
+
+    /*
+    * Generates a normal distribution random number between 0 and 1.
+    * @return Random double between 0 and 1.
+    */
+    double Deck::random() {
+        mt19937 generator;
+        generator.seed(time(0));
+        uniform_real_distribution<double> cardRand(0, 1);
+        return cardRand(generator);
+    }
---
+++ Solitaire.cpp
@@ -0,0 +1,395 @@
+#include <list>
+#include <vector>
+#include <random>
+#include <time.h>
+
+#include "Card.h"
+#include "Deck.h"
+#include "Gameboard.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <unistd.h>
+
+using namespace std;
+
+/**
+ * This class is the main solitaire game class. It uses the classes Deck, Card, and Tableau to
+ * execute a solitaire simulation game.
+ * @author Colin Tate from Jacob's Solitaire.java
+ * @version February 17 2019
+ */
+class Solitaire {
+
+    /**
+     * Solitaire constructor. Consists of creating a new
+     * deck of cards, then shuffling those cards, and then
+     * executing a the solitaire game.
+	 * @return A solitaire game object to be played
+     */
+public:
+
+Solitaire() {
+        Deck d = Deck();
+        vector<Card> *deck = d.getDeck();
+
+        double overall_Count = 0;
+        double overall_Probability = 0;
+        double overall_Time = 0;
+        vector<double> *probabilities = new vector<double>();
+        vector<double> *execution = new vector<double>();
+        int NumberOfSimulations = 100;
+
+        for(int j = 0; j<NumberOfSimulations;j++) {
+
+            double i = 0;
+            double counter = 0;
+            double seconds = 0;
+
+            while (i < 2000) {
+
+                time_t begin = clock();
+                i++;
+                srand(time(0));
+                //Randomly shuffles the deck
+                //cout << "SHUFFLED DECK\n" << endl;
+                d.shuffle();
+                deck = d.getDeck();
+                GameBoard t = GameBoard(d);
+                GameBoard temp = t;
+                //t.printGameBoard();
+
+                //Shuffles the deck for a GUARENTEED WIN
+                //          cout << "SHUFFLED DECK -- GUARENTEED TO WIN\n";
+                //          d.shuffle_WIN2();
+                //          deck = d.getDeck();
+                //          GameBoard t = GameBoard(d);
+
+                //          //Shuffles the deck for a GUARENTEED LOSS
+                //          cout << "SHUFFLED DECK -- GUARENTEED TO LOSE\n";
+                //          d.shuffle_LOSE();
+                //          deck = d.getDeck();
+                //          GameBoard t = GameBoard(d);
+
+                //A loop that executes the solitaire game
+                while (*t.getStockCounter() < 3) {
+                    bool tab = true;
+                    bool stock = false;
+                    while (tab) {
+                        tab = checkTableau(&t);
+                        //t.printGameBoard();
+                    }
+                    while (!stock) {
+                        stock = checkStock(&t);
+                        //t.printGameBoard();
+                    }
+                }
+
+                //If all of the cards are in the destination piles YOU WIN
+                vector<vector<Card>> *destination = t.getDestination();
+                if (destination->at(0).size() == 13 && destination->at(1).size() == 13 &&
+                    destination->at(2).size() == 13 && destination->at(3).size() == 13) {
+                    //cout << "YOU WIN" << endl;
+                    //temp.printGameBoard();
+                    //t.printGameBoard();
+                    counter++;
+                }
+                //else
+                //  cout << "YOU LOSE" << endl;
+                time_t end = clock();
+                seconds = seconds + (double) (end - begin) / CLOCKS_PER_SEC;
+            }
+            cout << "\n--------------------------------------------------------------------------------------------------------------------------------------------------------";
+            cout << "\nGAME: " << j +1 ;
+            cout << "\nWINS: " << counter;
+            cout << "\nPROBABILITY: " << counter / i << " (" << (counter / i) * 100 << "%)";
+            cout << "\nAVERAGE EXECUTION TIME PER SIMULATION: " << seconds / i << " seconds";
+
+            probabilities->push_back(counter/i);
+            execution->push_back(seconds/i);
+
+            overall_Count += counter;
+            overall_Probability += counter / i;
+            overall_Time += seconds / i;
+        }
+        cout << "\n********************************************************************************************************************************************************";
+        cout << "\nOVERALL";
+        cout << "\nAVERAGE WINS: " << (int) overall_Count/NumberOfSimulations;
+        cout << "\nAVERAGE (Mean) PROBABILITY: " << overall_Probability/NumberOfSimulations << " (" << overall_Probability*(100/NumberOfSimulations) << "%)";
+        cout << "\nAVERAGE (Mean) EXECUTION TIME PER SIMULATION: " << overall_Time/NumberOfSimulations << " seconds";
+
+        double prob_sd = 0;
+        double ex_sd = 0;
+        for(int j = 0;j<probabilities->size();j++){
+            prob_sd += pow(probabilities->at(j)-(overall_Probability/100),2);
+            ex_sd += pow(execution->at(j)-(overall_Time/100),2);
+        }
+
+        prob_sd = sqrt(prob_sd/100);
+        ex_sd = sqrt(ex_sd/100);
+
+        cout << "\nSTANDARD DEVIATION PROBABILITY: " << prob_sd;
+        cout << "\nSTANDARD DEVIATION EXECUTION TIME PER SIMULATION: " << ex_sd;
+        cout << "\n********************************************************************************************************************************************************";
+    }
+
+    /**
+     * This method is used for checking the stock pile. First, it checks to see if the card on the stock
+     * pile can be moved to a destination pile. It then checks to see if it can be moved to somewhere on
+     * the tableau.
+     * @param t the tableau GameBoard object for the game
+     * @return returning false will cause the while loop above to loop again. This means, if a stock
+     *		card was placed in a destination pile, it loops again, since nothing was placed on the bottom
+     *		tableau, so that doesn't need to be checked again for moves. Returning true means a stock card
+     *		was placed on the tableau, which will cause the loop to stop since we now need to look over the
+     *		tableau and see if we can do any moves down there
+     */
+    bool checkStock(GameBoard *t) {
+        vector<vector<Card>> *board = t->getTableau();
+        vector<Card> *stock = t->getStock();
+        vector<Card> *discard = t->getDiscard();
+        vector<vector<Card>> *destination = t->getDestination();
+
+        //If the stock is not empty:
+        if (!stock->empty()) {
+            Card compC = stock->at(0);
+            compC.makeVisible();
+
+            //Check to see if the stock card can be placed in any of the 4 destination piles
+            for (int j = 0; j < 4; j++) {
+                vector<Card> &dest = destination->at(j);
+                if (!dest.empty()) {
+                    Card card = dest.at(dest.size()-1);
+                    bool b = t->validDestinationMove(compC, card);
+                    //If it is a valid move, move the stock card, and flip a new stock card over
+                    if (b) {
+                        stockMoveCard(stock,&dest,&compC,t);
+                        return true;
+                    }
+                }
+
+                    //If the stock card is an Ace, it can be automatically placed in one of the empty destination piles
+                else if (compC.getRank() == 1) {
+                    stockMoveCard(stock,&dest,&compC,t);
+                    return true;
+                }
+            }
+
+            //Check if it can be placed on any of the bottom columns
+            for (int i = 0; i < 7; i++) {
+                vector<Card> &col = board->at(i);
+                if (!col.empty()) {
+                    Card c = col.at(col.size()-1);
+                    if (c.getVisibility()) {
+                        bool b = t->validTableauMove(compC, c);
+                        if (b) {
+                            //If it is a valid move, move the stock card, and flip a new stock card over
+                            stockMoveCard(stock,&col,&compC,t);
+                            return true;
+                        }
+                    }
+                }
+                    //If the tableau column is empty, then the stock card can be placed in the empty column
+                else {
+                    stockMoveCard(stock,&col,&compC,t);
+                    return true;
+                }
+            }
+
+            //If the card couldn't be placed, remove the card from the stock and put it into the Discard Pile
+            compC.makeInvisible();
+            discard->push_back(compC);
+            stock->erase(stock->begin());
+
+            //If the stock isn't empty, flip over a new card
+            if (!stock->empty()) {
+                stock->at(0).makeVisible();
+                return false;
+            }
+                //If the stock is empty, the counter is incremented, and the discard pile become the stock again
+            else {
+                //cout << "COUNTER INCREMENTED" << endl;
+                t->incrementStockCounter();
+                t->swapStockAndDiscard();
+                if (*t->getStockCounter() < 3)
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+            //If the stock is empty, the counter is incremented, and the discard pile become the stock again
+        else if (*t->getStockCounter() < 3) {
+            //cout << "COUNTER INCREMENTED" << endl;
+            t->incrementStockCounter();
+            t->swapStockAndDiscard();
+            return false;
+        } else
+            return true;
+    }
+
+    /**
+     *This method is responsible for simply moving a stoack card to the new pile it belongs on
+     * @param stock
+     * @param dest
+     * @param compC
+     * @param t
+     */
+    void stockMoveCard(vector<Card> *stock, vector<Card> *dest,Card *compC, GameBoard *t){
+        stock->erase(stock->begin());
+        dest->push_back(*compC);
+        if (!stock->empty())
+            stock->at(0).makeVisible();
+    }
+
+    /**
+     * This method is used for checking the tableau. First, it checks to see if any of the cards on the tableau can be moved
+     * to a destination pile. It then checks to see if any of the cards on the tableau can be moved to another spot on the tableau
+     * @param t the tableau object for the game
+     * @return returning true means that a card on the tableau was either moved to a destination pile, or moved to another spot on the
+     * tableau. Either way, the main loop above continues, and the tableau is rechecked for new possible moves. Returning false means
+     * that there were no valid moves on the tableau.
+     */
+    bool checkTableau(GameBoard *t) {
+        vector<vector<Card>> *board = t->getTableau();
+        vector<vector<Card>> *destination = t->getDestination();
+
+        //Checks if any of the cards below can be moved to the Top
+        for (int i = 0; i < 7; i++) {
+            //gets a column of the tableau
+            vector<Card> &col = board->at(i);
+            if (!col.empty()) {
+                //each ending card in each column is checked to see if it can be moved above
+                Card colCard = col.at(col.size() - 1);
+                for (int j = 0; j < 4; j++) {
+                    vector<Card> &dest = destination->at(j);
+                    if (colCard.getVisibility()) {
+                        if (!dest.empty()) {
+                            Card destCard = dest.at(dest.size() - 1);
+                            bool b = t->validDestinationMove(colCard, destCard);
+                            if (b) {
+                                dest.push_back(col.at(col.size() - 1));
+                                col.erase(col.begin() + col.size() - 1);
+                                if (!col.empty())
+                                    col.at(col.size() - 1).makeVisible();
+                                return true;
+                            }
+                        } else if (colCard.getRank() == 1) {
+                            col.erase(col.begin() + col.size() - 1);
+                            dest.push_back(colCard);
+                            if (!col.empty())
+                                col.at(col.size() - 1).makeVisible();
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Check if any of the bottom cards can be moved to eachother
+        for (int i = 0; i < 6; i++) {
+            //gets a column of the tableau
+            vector<Card> &col = board->at(i);
+            if (!col.empty()) {
+                for (int k = 0; k < col.size(); k++) {
+                    //Checks each card in that column of the tableau
+                    Card colCard = col.at(k);
+                    if (colCard.getVisibility()) {
+                        for (int j = i + 1; j < 7; j++) {
+                            //gets a different column of the tableau
+                            vector<Card> &dest = board->at(j);
+                            if (!dest.empty()) {
+                                for (int m = 0; m < dest.size(); m++) {
+                                    //checks each card in that other column of the tableau
+                                    Card destCard = dest.at(m);
+                                    if (destCard.getVisibility()) {
+                                        //Checks both ways to see if either move is valid
+                                        bool a = t->validTableauMove(colCard, destCard);
+                                        bool b = t->validTableauMove(destCard, colCard);
+
+                                        //I chose to only allow cards to swap, if this card is not part of a run already
+                                        //for example a 2 of hearts can be moved to a 3 only if the 2 isn't already on another 3
+                                        if (a && m == (dest.size() - 1) &&
+                                            (k == 0 || !(col.at(k - 1).getVisibility()))) {
+                                            int size = col.size();
+                                            for (int n = k; n < size; n++) {
+                                                dest.push_back(col.at(k));
+                                                col.erase(col.begin() + k);
+                                            }
+                                            if (!col.empty())
+                                                col.at(col.size() - 1).makeVisible();
+                                            return true;
+                                        }
+                                            //I chose to only allow cards to swap, if this card is not part of a run already
+                                            //for example a 2 of hearts can be moved to a 3 only if the 2 isn't already on another 3
+                                        else if (b && k == (col.size() - 1) &&
+                                                 (m == 0 || !(dest.at(m - 1).getVisibility()))) {
+                                            int size = dest.size();
+                                            for (int n = m; n < size; n++) {
+                                                col.push_back(dest.at(m));
+                                                dest.erase(dest.begin() + m);
+                                            }
+                                            if (!dest.empty())
+                                                dest.at(dest.size() - 1).makeVisible();
+                                            return true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //If no cards can be moved onto one another, this loop checks to see if there are any empty rows
+        //and if there are, it moves a stack onto an empty row to reveal a non-visible card
+        for (int i = 0; i < 7; i++) {
+            //gets a column of the tableau
+            vector<Card> &emptyCol = board->at(i);
+            //If the row is empty, we want to then move a visible stack of cards to this to turn over a non-visble card
+            if (emptyCol.empty()) {
+
+                for (int j = 0; j < 7; j++) {
+                    vector<Card> &col = board->at(j);
+
+                    //Find the first row that contains cards in it
+                    if (!col.empty()) {
+                        if(!col.at(0).getVisibility()) {
+
+                            for (int m = 0; m < col.size(); m++) {
+                                Card destCard = col.at(m);
+                                if (destCard.getVisibility()) {
+                                    int size = col.size();
+                                    for (int n = m; n < size; n++) {
+                                        emptyCol.push_back(col.at(m));
+                                        col.erase(col.begin() + m);
+                                    }
+                                    col.at(col.size() - 1).makeVisible();
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+/**
+ * Main method for creating a new Solitaire object and running the game
+ * @param argc int number of arguments
+ * @param argv argument variable
+ */
+int main(int argc, char** argv) {
+    Solitaire s = Solitaire();
+
+    return 0;
+}
